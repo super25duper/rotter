51f4d3fead92d08d4f344fcc4cd4a81851377b1c
super25duper <yehonami2500@gmail.com>
2025-11-24 17:51:01 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-console.log('Hello World!');
+function om() {
+  const menu = document.getElementById("pm");
+  const os = document.getElementById("os");
+  const cm = document.getElementById("cm");
+  
+  if (menu.style.right === "-270px") {
+    menu.style.right = "0vw";
+    os.style.opacity = "1";
+    cm.style.zIndex = "1";
+  } else {
+    menu.style.right = "-270px";
+    os.style.opacity = "0";
+  }
+}
